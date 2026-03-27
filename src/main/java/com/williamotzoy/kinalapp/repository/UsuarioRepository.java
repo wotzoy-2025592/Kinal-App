fe32a87c2bdbfa5136aa7d8fb909dc7b74dc0729
wotzoy-2025592 <wotzoy-2025592@kinal.edu.gt>
2026-03-27 02:27:17 -0600

    fix: wotzoy-2025592 add method 'findByEstado' to list users by status in repository

--- src/main/java/com/williamotzoy/kinalapp/repository/UsuarioRepository.java
+++ src/main/java/com/williamotzoy/kinalapp/repository/UsuarioRepository.java
@@ -3,5 +3,8 @@ package com.williamotzoy.kinalapp.repository;
 import com.williamotzoy.kinalapp.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+    List<Usuario> findByEstado (Long estado);
 }
