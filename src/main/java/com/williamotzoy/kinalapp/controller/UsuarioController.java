e17d5186ba276357e0b5f7db7dcc703b84bff243
wotzoy-2025592 <wotzoy-2025592@kinal.edu.gt>
2026-04-15 08:54:10 -0600

    fix: wotzoy-2025592 updated request mapping to avoid route conflicts between controllers(usuarios-textito/usuarios)

--- src/main/java/com/williamotzoy/kinalapp/controller/UsuarioController.java
+++ src/main/java/com/williamotzoy/kinalapp/controller/UsuarioController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/usuarios")
+@RequestMapping("/usuarios-textito")
 public class UsuarioController {
 
     private final IUsuarioService usuarioService;
