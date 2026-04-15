9cba0ca42ec11a352d2fc6870eee63fca69bbe8f
wotzoy-2025592 <wotzoy-2025592@kinal.edu.gt>
2026-04-15 02:41:28 -0600

    fix: wotzoy-2025592 refactored menu controller mapping to simplify route handling and ensure the main view (index.html) loads correctly from the root path ('/')

--- src/main/java/com/williamotzoy/kinalapp/controller/MenuViewController.java
+++ src/main/java/com/williamotzoy/kinalapp/controller/MenuViewController.java
@@ -5,10 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/main")
 public class MenuViewController {
 
-    @GetMapping("/menu-principal")
+    @GetMapping("/")
     public String menu() {
         return "index";
     }
