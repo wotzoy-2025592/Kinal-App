532000b760eec20aca5dc125e7ed81912f8545ad
wotzoy-2025592 <wotzoy-2025592@kinal.edu.gt>
2026-04-17 22:48:41 -0600

    fix: wotzoy-2025592 add new option to search name user in 'UsuarioRepository'

--- src/main/java/com/williamotzoy/kinalapp/repository/UsuarioRepository.java
+++ src/main/java/com/williamotzoy/kinalapp/repository/UsuarioRepository.java
@@ -4,7 +4,9 @@ import com.williamotzoy.kinalapp.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     List<Usuario> findByEstado (Long estado);
+    Optional<Usuario> findByUserName(String userName);
 }
